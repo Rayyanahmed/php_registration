40296010682e9664d52a4b9adfcdaf17270f0fe5
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-25 01:00:59 -0500

    added data fetching function

--- functions/db.php
+++ functions/db.php
@@ -4,15 +4,22 @@ $con = mysqli_connect('localhost', 'root', '', 'login_db');
 
 // Add functions that have to do with database
 
+
 function escape($string) {
 	global $con;
-	
+
 	return mysqli_real_escape_string($con, $string)
 }
 
 function query($query) {
 	global $con;
 	return mysqli_query($con, $query);
+}
+
+function fetch_array($result) {
+	global $con;
+
+	mysqli_fetch_array($result);
 }
 
 ?>
