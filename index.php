379228359be70c3d09f52b3e34a8418fd2c06b9e
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-25 10:43:00 -0500

    test

--- index.php
+++ index.php
@@ -6,6 +6,15 @@
 		<h1 class="text-center"> Home Page</h1>
 	</div>
 
+	<?php
+
+	$sql = "SELECT * FROM users";
+	$result = query($sql);
+	confirm($result);
+	echo row_count($result);
+	// Outputs 1, only have 1 datset in database right now
+
+	 ?>
 
 
 <?php include("includes/footer.php"); ?>
