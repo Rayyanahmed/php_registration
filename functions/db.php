4e978c00b2e5920b404c89d1c734c0d1c15ec7ec
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-25 00:56:46 -0500

    added query function

--- functions/db.php
+++ functions/db.php
@@ -4,6 +4,9 @@ $con = mysqli_connect('localhost', 'root', '', 'login_db');
 
 // Add functions that have to do with database
 
-
+function query($query) {
+	global $con;
+	return mysqli_query($con, $query);
+}
 
 ?>
