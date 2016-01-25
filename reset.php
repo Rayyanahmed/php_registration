863c40b6870485bdfe121b3d64a053a57ae29f29
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-24 23:24:41 -0500

    finished refactoring

--- reset.php
+++ reset.php
@@ -1,17 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-	<link rel="stylesheet" href="css/bootstrap.css">
-	<link rel="stylesheet" href="css/styles.css">
-	<script src="js/jquery.js"></script>
-	<script src="js/bootstrap.js"></script>
-	<script src="js/scripts.js"></script>
-</head>
-<body>
-	
-<div class="container">
+<?php include("includes/header.php"); ?>
+<?php include("includes/nav.php"); ?>
+
 
 	<div class="row">
 
@@ -53,11 +42,5 @@
 				</div>
 			</div>
 		</div>
-	</div>
 
-
-
-
-	
-</body>
-</html>
+<?php include("includes/footer.php"); ?>
