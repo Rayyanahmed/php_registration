085854ffb7441ef39f583116bca9cea92a5bd656
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-25 00:57:56 -0500

    added escape function

--- functions/db.php
+++ functions/db.php
@@ -4,6 +4,12 @@ $con = mysqli_connect('localhost', 'root', '', 'login_db');
 
 // Add functions that have to do with database
 
+function escape($string) {
+	global $con;
+	
+	return mysqli_real_escape_string($con, $string)
+}
+
 function query($query) {
 	global $con;
 	return mysqli_query($con, $query);
