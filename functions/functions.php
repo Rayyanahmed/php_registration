30de7e9e434867d1a8d809cdfa1555e6a26731fa
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-25 10:56:55 -0500

    added display message function

--- functions/functions.php
+++ functions/functions.php
@@ -20,4 +20,12 @@ function set_message($message) {
 }
 
 
+function display_message() {
+	if(isset($_SESSION['message'])) {
+		echo $_SESSION['message'];
+		unset($_SESSION['message']);
+	}
+}
+
+
 ?>
